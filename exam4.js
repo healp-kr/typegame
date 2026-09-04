86b7ac7f81014c489f1c5954e1997e7ca2846a49
cacascas <dlcodud156@gmail.com>
2026-09-04 14:26:43 +0900

    fix:exam.js

--- exam4.js
+++ exam4.js
@@ -647,13 +647,13 @@ function updateHud() {
         el.barTime.classList.remove('is-urgent');
     }
 
-    state.typedValue = '';
-    state.lastCommittedValue = '';
-    state.awaitingNext = false;
-    elements.typingInput.value = '';
-    elements.practiceHint.textContent = '현재 구간을 정확히 입력한 뒤 Enter를 누르세요.';
-    updateSegmentMeta();
-    renderTypingProgress();
+    // state.typedValue = '';
+    // state.lastCommittedValue = '';
+    // state.awaitingNext = false;
+    // elements.typingInput.value = '';
+    // elements.practiceHint.textContent = '현재 구간을 정확히 입력한 뒤 Enter를 누르세요.';
+    // updateSegmentMeta();
+    // renderTypingProgress();
 }
 
 /* =========================================================
